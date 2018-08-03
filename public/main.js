64ea248e4a1802f289b44121cdf082d76e6fcc17
Daniel Somoano <dnsomoano@gmail.com>
2018-08-02 21:31:04 -0400

    setup up two card hand, now must setup 2nd hand

--- public/main.js
+++ public/main.js
@@ -1,5 +1,77 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
 
-document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('DOMContentLoaded', main)
+
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
+// document.addEventListener('DOMContentLoaded', main)
+// Starting arrays
+const suits = [ 'clubs', 'diamonds', 'hearts', 'spades' ];
+const ranks = [ 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King' ];
+// Parallel array
+const valueArray = [ 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 ];
+
+let deck = [];
+
+// Creates array of deck
+const createDeck = () => {
+	// First loop = 13 * Second loop 4 = 52 loops
+	// First loop
+	deck = [];
+	for (let i = 0; i < ranks.length; i++) {
+		// second loop
+		for (let j = 0; j < suits.length; j++) {
+			// Bucket for temporary values
+			deck.push({
+				value: valueArray[i],
+				rank: ranks[i],
+				suit: suits[j]
+			});
+			// Call list of cards
+			console.log(deck);
+		}
+	}
+	// Call length of array to confirm 52 cards
+	console.log(deck.length);
+};
+
+// Fisher-Yates Shuffle
+let shuffle = () => {
+	// For loop will loop 52x
+	for (let i = 0; i < deck.length; i++) {
+		const j = Math.floor(Math.random() * i);
+		// temporary bucket for array deck
+		const temp = deck[j];
+		// value which dumped into temporary is overwritten
+		deck[j] = deck[i];
+		// temporary bucket overwrites the previous array that overwrote
+		deck[i] = temp;
+	}
+	// pop method removes the last element in the array deck
+};
+
+const resultCard = () => {
+	for (let index = 0; index < 2; index++) {
+		let hand = deck.pop();
+		// debug message for pop value
+		console.log(hand);
+		// Create new line element for result
+		const newLi = document.createElement('li');
+		// New line writes the last element named hand
+		newLi.textContent = hand.rank + " of " + hand.suit;
+		// Calls new line back to the DOM
+		document.querySelector('#random-result').appendChild(newLi);
+	}
+};
+
+const main = () => {
+	createDeck();
+	shuffle();
+	resultCard();
+};
+
+// Event listeners
+document.querySelector('.hit-button').addEventListener('click', main);
---
+++ public/requirements.md
@@ -0,0 +1,41 @@
+// let cardPath "./images/"+hand+;
+// Image.setAttribute("src", cardPath)
+// transform: rotate()
+//transform: spin()
+
+------
+
+// create an object with properties
+const _usr = {
+    name: "borg"
+    occupation:
+    age
+}
+
+// name of funct is key value
+const card = {
+    value: ""
+    suit: ""
+    rank: ""
+}
+
+// call object
+card.rank
+
+// update prop
+card.rank = "new rank"
+
+---------------------
+
+- [ ] user can buy multiple tickets
+    - [ ] add textbox to see how many tickets they bought
+    - [ ] store amount paid, per person
+    - [ ] 
+
+const _ticketsBought = parseInt(document.querselector('# of tickets box).value);
+
+const buyer = {
+    name: document.querySelector ('name-textbox').value
+    tickets: document.queryselector('# of tickets box').value
+    amountPaid: 
+}
